fd35330c203ef1dd8a39876ff6275eea58bbffad
Islem Fortas <fortas.yacoub85@gmail.com>
2024-12-01 15:59:51 +0100

    contact page update

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -2,24 +2,28 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    
-    <div className="flex justify-center items-center 
-    min-h-screen bg-white">
-      
-      <div>
-      <div className='flex  bg-uni-green mt-[50px] w-full h-full'>banner</div>
-      <div className='flex justify-center md:justify-normal '>
+<>
+<>
+      <div className="w-full h-80 md:h-[400px] bg-cyan-400"></div>
+
+      <div className="mx-auto w-full felx flex-col p-4  bg-green-500 mt-[-20px]">
+        <h1 className='text-xl'>contact us</h1>
+      </div>
+    </>
+    {/* <div className=' bg-uni-green text-white mt-[50px] w-full h-full'>banner</div> */}
+    <div className="flex justify-center items-center bg-white">
+      <div className=' flex flex-col  '>
+      <div className=' justify-center md:justify-normal '>
         Contact Us
       </div>
       <div className="flex flex-col justify-center items-center
-       m-3 bg-gray-200 text-black w-[350px] h-[500px] 
-       md: w-[900px] h-[700px] rounded-lg shadow-lg">
+       m-3 bg-gray-200 text-black w-[150px] h-[500px]  md:w-[900px] h-[600] rounded-lg shadow-lg">
       </div>
     <div>
     </div>
   </div>
 </div>
-
+</>
   )
 }
 
