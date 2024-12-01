c226d1b841d42e93a009eee1ffacc916be971657
Islem Fortas <fortas.yacoub85@gmail.com>
2024-12-01 13:28:27 +0100

    contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className="flex justify-center items-center min-h-screen  bg-gray-200">
+  <div className="flex flex-col justify-center items-center m-3 bg-gradient-to-r from-blue-500 to-green-500 text-black w-[400px] h-[500px] rounded-lg shadow-lg">
+    contact
+  </div>
+</div>
+
   )
 }
 
