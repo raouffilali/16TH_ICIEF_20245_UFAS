61f8d1961bb43f7c72289022e78bfff7ab0b6927
Abderraouf FILALI <contact@abderraouf-filali.tech>
2024-12-01 07:37:14 +0100

    Add constants for speakers team and sponsors

---
+++ src/constants/speakers-team.ts
@@ -0,0 +1,22 @@
+export const SpeakersTeam = [
+  {
+    name: "Speaker 1",
+    image: "speaker1.png",
+    designation: "Web Developer",
+  },
+  {
+    name: "Speaker 2",
+    image: "speaker2.png",
+    designation: "Mobile Developer",
+  },
+  {
+    name: "Speaker 3",
+    image: "speaker3.png",
+    designation: "Designer",
+  },
+  {
+    name: "Speaker 4",
+    image: "speaker4.png",
+    designation: "Tester",
+  },
+];
