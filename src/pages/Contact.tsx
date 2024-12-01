460a20191110e37c4741044f2d3034bb628fd9e6
Islem Fortas <fortas.yacoub85@gmail.com>
2024-12-01 14:19:37 +0100

    contact page banner

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -2,9 +2,21 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div className="flex justify-center items-center min-h-screen  bg-gray-200">
-  <div className="flex flex-col justify-center items-center m-3 bg-gradient-to-r from-blue-500 to-green-500 text-black w-[400px] h-[500px] rounded-lg shadow-lg">
-    contact
+    
+    <div className="flex justify-center items-center 
+    min-h-screen bg-white">
+      
+      <div>
+      <div className='flex  bg-uni-green mt-[50px] w-full h-full'>banner</div>
+      <div className='flex justify-center md:justify-normal '>
+        Contact Us
+      </div>
+      <div className="flex flex-col justify-center items-center
+       m-3 bg-gray-200 text-black w-[350px] h-[500px] 
+       md: w-[900px] h-[700px] rounded-lg shadow-lg">
+      </div>
+    <div>
+    </div>
   </div>
 </div>
 
