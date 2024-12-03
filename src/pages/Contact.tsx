9b904d67a10ca98ede0a07faa8848c993df42b50
Islem Fortas <fortas.yacoub85@gmail.com>
2024-12-03 15:58:38 +0100

    contact form updated 3:58

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -6,14 +6,23 @@ import AnimatedBanner from "@/custom/components/AnimatedBanner";
 const Contact = () => {
   return (
     <>
-      
-       <AnimatedBanner/>
+      <AnimatedBanner />
 
       {/* Contact Form Section */}
-      <div className="flex justify-center items-center w-full bg-white text-black h-auto p-6 rounded-lg shadow-lg">
-        <div className="flex flex-col ">
-          <div className="w-full  h-full  bg-gradient-to-t"></div>
-          <form className="flex flex-col w-full max-w-[400px] sm:max-w-[600px] md:max-w-[700px]">
+      <div className="flex justify-center  items-center w-full bg-white text-black h-auto p-2">
+        <div className="flex flex-col md:flex-row rounded-lg shadow-lg bg-slate-300 p-4 md:justify-between">
+          <div className="w-full h-auto mx-1 bg-gradient-to-bl from-[#0f5a08] via-[#709c1f] to-[#0f5a08] text-white p-5 justify-between rounded-lg">
+            <h1 className="text-xl font-extrabold">EVENT VENUE:</h1>
+            <p>Galleria mall conference</p>
+            <h1>ADDRESS:</h1>
+            <p>19 By Pass NR, Bali, Indonesia, BC 22196</p>
+            <h1>RECEPTION INFO:</h1>
+            <p>Booking: (+62) 1919-2020</p>
+            <h1>Email:</h1>
+            <p>eventen@mail.com</p>
+            <p>Get Directions</p>
+          </div>
+          <form className="flex flex-col mx-1 w-full max-w-[400px] sm:max-w-[600px] md:max-w-[700px]">
             {/* Name Input */}
             <label
               className="mb-2 text-lg font-medium text-gray-700"
