817b658124eb9c2fd242c088fd1a1a6f0b14addd
Raouf Filali <filaliabderaouf@yahoo.com>
2024-12-10 16:57:09 +0100

    Update height of map embed in Lp_venue component for better display

--- src/pages/Home/SubComponent/Lp_venue.tsx
+++ src/pages/Home/SubComponent/Lp_venue.tsx
@@ -44,7 +44,7 @@ const Lp_venue = () => {
             title="Event Location"
             src="https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d3923.9911245374446!2d5.362661276473721!3d36.198541372424756!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e6!4m0!4m5!1s0x12f33e5a99993c61%3A0xbf33966b62b1a5ec!2zNTlXNitYOUcg2YPZhNmK2Kkg2KfZhNin2YLYqti12KfYr9iMINmH2LbYqNipINin2YTYqNin2LIsIFPDqXRpZg!3m2!1d36.197181!2d5.3609165!5e0!3m2!1sen!2sdz"
             width="100%"
-            height="390"
+            height="375"
             style={{ border: 0 }}
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
