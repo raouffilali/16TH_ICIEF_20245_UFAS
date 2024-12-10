e0353cee6c6ada811912cf486848da32527c17d2
Islem Fortas <fortas.yacoub85@gmail.com>
2024-12-10 10:57:32 +0100

    importtante dates and high committee updates

--- src/pages/Home/SubComponent/ImportantDatesHighCommittee.tsx
+++ src/pages/Home/SubComponent/ImportantDatesHighCommittee.tsx
@@ -1,4 +1,6 @@
 import { Conf } from '@/assets'
+import { highcommittees } from '@/constants/Highcommittee';
+import { Calendar } from 'lucide-react';
 
 const ImportantDatesHighCommittee = () => {
   return (
@@ -16,23 +18,39 @@ const ImportantDatesHighCommittee = () => {
         Important Dates
       </h1>
 
-      <div className="relative flex flex-col items-center w-full text-white ">
-        <div className="w-auto flex flex-col items-center md:flex-row md:gap-6 divide-y-2 md:divide-x-2 md:divide-y-0  ">
-          <div className="w-full h-full flex flex-col items-center md:pl-9 ">
-            <h1 className="text-4xl font-bold ">05/12/2024</h1>
-            <p className="text-center pb-4 md:pt-8">Submission Opening</p>
+      <div className="relative flex flex-col items-center w-full text-white border-b-2 pb-8 border-opacity-50">
+        <div className="flex flex-col items-center xl:w-full xl:flex-row divide-y-2 xl:divide-x-2 xl:divide-y-0  ">
+          <div className="w-full h-full flex flex-col items-center ">
+            <h1 className="w-full text-4xl font-bold text-center">
+              05/12/2024
+            </h1>
+            <p className=" w-full text-center text-md font-bold pb-4 md:pt-8">
+              Submission Opening
+            </p>
           </div>
-          <div className="w-full h-full flex flex-col items-center md:pl-9">
-            <h1 className="text-4xl font-bold pt-4 md:pt-0">05/12/2024</h1>
-            <p className="text-center pb-4 md:pt-8">Submission Opening</p>
+          <div className="w-full h-full flex flex-col items-center ">
+            <h1 className="w-full text-4xl font-bold pt-4 md:pt-0 text-center">
+              15/02/2025
+            </h1>
+            <p className="w-full text-center text-md font-bold pb-4 md:pt-8">
+              Full Paper Submission Deadline
+            </p>
           </div>
-          <div className="w-full h-full flex flex-col items-center md:pl-9">
-            <h1 className="text-4xl font-bold pt-4 md:pt-0">05/12/2024</h1>
-            <p className="text-center pb-4 md:pt-8">Submission Opening</p>
+          <div className="w-full h-full flex flex-col items-center ">
+            <h1 className="w-full text-4xl font-bold pt-4 md:pt-0 text-center">
+              15/03/2025
+            </h1>
+            <p className="w-full text-center text-md font-bold pb-4 md:pt-8">
+              Scientific Committee Decision
+            </p>
           </div>
-          <div className="w-full h-full flex flex-col items-center md:pl-9">
-            <h1 className="text-4xl font-bold pt-4 md:pt-0">05/12/2024</h1>
-            <p className="text-center pb-4 md:pt-8">Submission Opening</p>
+          <div className="w-full h-full flex flex-col items-center ">
+            <h1 className="w-full text-4xl font-bold pt-4 md:pt-0 text-center">
+              13-15/05/2025
+            </h1>
+            <p className="w-full text-center text-md font-bold pb-4 md:pt-8 ">
+              Conference Dates
+            </p>
           </div>
         </div>
       </div>
@@ -40,6 +58,35 @@ const ImportantDatesHighCommittee = () => {
       <h1 className="relative text-center md:text-start text-5xl font-bold text-white px-4 py-8 my-4">
         High Committee
       </h1>
+      <div className="relative flex flex-col items-center md:flex-row mx-4 gap-4">
+        <div className="relative p-5 rounded-lg  shadow-uni-green2 w-full h-full mx-auto-50 flex flex-col gap-4 md:flex-row items-center ">
+          {highcommittees.map((Highcommittee) => (
+            <div
+              key={Highcommittee.id}
+              className="w-full flex flex-col h-full md:max-h-[190px] items-center md:flex-row md:items-start bg-black bg-opacity-50 to-white
+                  p-4 rounded-lg md:w-[calc(50%-16px)] mb-6 mt-6 md:mb-0 md:mt-0 transition-all ease-in-out duration-300 transform hover:scale-105 "
+            >
+              {Highcommittee.image && (
+                <img
+                  src={Highcommittee.image}
+                  alt={Highcommittee.name}
+                  className="w-full h-auto max-w-[150px] bg-white shadow-2xl object-cover rounded-lg mb-4 animate-slidleft800"
+                />
+              )}
+              <div className="w-full h-full flex flex-col items-center md:items-start p-2 animate-slideright800">
+                <h3 className="px-2 text-xl font-bold text-white text-center md:text-start">
+                  {Highcommittee.name}
+                </h3>
+                {Highcommittee.description && (
+                  <div className="text-center md:text-start mt-2 h-24 overflow-y-auto overflow-hidden text-white border-0 text-sm px-2 rounded-md scrollbar-thin scrollbar-thumb-uni-green2 scrollbar-track-gray-100 bg-transparent">
+                    {Highcommittee.description}
+                  </div>
+                )}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
