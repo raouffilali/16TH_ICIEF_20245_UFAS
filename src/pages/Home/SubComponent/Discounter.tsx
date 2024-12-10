f40fdf1b3a22c4e6a17a0cd2d589f09efe582bd9
Fortas Nour El Islem Yaakoub <fortas.yacoub85@gmail.com>
2024-12-10 22:15:11 +0100

    Final Touche

--- src/pages/Home/SubComponent/Discounter.tsx
+++ src/pages/Home/SubComponent/Discounter.tsx
@@ -37,22 +37,22 @@ const CountdownTimer: React.FC<CountdownTimerProps> = ({ targetDate }) => {
   }
 
   return (
-    <div className="flex flex-row justify-center items-center md:gap-14 bg-white text-uni-green2 p-2 md:p-6 shadow-[0_0_20px_10px_rgba(15,90,8,0.3)] rounded-2xl animate-slidleft800">
+    <div className="flex flex-row justify-center items-center gap-5 md:gap-14 bg-white text-uni-green2 p-2 md:p-6 shadow-[0_0_20px_10px_rgba(15,90,8,0.3)] rounded-2xl animate-slidleft800">
       <div className="text-center">
         <h1 className="text-xl md:text-4xl font-bold">{timeLeft.days}</h1>
-        <p className="text-sm text-gray-800 font-semibold">Days</p>
+        <p className="text-xs md:text-sm  text-gray-800 font-semibold">Days</p>
       </div>
       <div className="text-center">
         <h1 className="text-xl md:text-4xl font-bold">{timeLeft.hours}</h1>
-        <p className="text-sm text-gray-800 font-semibold">Hours</p>
+        <p className="text-xs md:text-sm text-gray-800 font-semibold">Hours</p>
       </div>
       <div className="text-center">
         <h1 className="text-xl md:text-4xl font-bold">{timeLeft.minutes}</h1>
-        <p className="text-sm text-gray-800 font-semibold">Minutes</p>
+        <p className="text-xs md:text-sm text-gray-800 font-semibold">Minutes</p>
       </div>
       <div className="text-center">
         <h1 className="text-xl md:text-4xl font-bold">{timeLeft.seconds}</h1>
-        <p className="text-sm text-gray-800 font-semibold">Seconds</p>
+        <p className="text-xs md:text-sm text-gray-800 font-semibold">Seconds</p>
       </div>
     </div>
   );
