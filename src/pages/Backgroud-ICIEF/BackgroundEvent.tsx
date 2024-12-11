239499c7d8f237f58152e2c7fa4caba063141434
Raouf Filali <filaliabderaouf@yahoo.com>
2024-12-11 14:09:03 +0100

    Refactor BackgroundEvent component: update text alignment and rename section headings for clarity

--- src/pages/Backgroud-ICIEF/BackgroundEvent.tsx
+++ src/pages/Backgroud-ICIEF/BackgroundEvent.tsx
@@ -1,7 +1,6 @@
 import { useEffect } from 'react';
 import { Bodytexture } from '@/assets'; // Adjust the import path as needed
 import { eventBackground } from '../../constants/eventBackground';
-import { ArrowRight, ArrowLeft } from 'lucide-react';
 
 const BackgroundEvent = () => {
   useEffect(() => {
@@ -23,7 +22,7 @@ const BackgroundEvent = () => {
           {eventBackground.title}
         </h1>
         {eventBackground.description.map((desc, index) => (
-          <p key={index} className="text-base md:text-lg md:italic mb-6 text-left w-full animate-slidein200">
+          <p key={index} className="text-base md:text-lg md:italic mb-6 text-left w-full animate-slidein200 md:text-justify">
             {desc}
           </p>
         ))}
@@ -51,10 +50,10 @@ const BackgroundEvent = () => {
           </tbody>
         </table>
         <h2 className="mt-12 text-2xl md:text-3xl font-bold mb-6 bg-gradient-to-r from-uni-green2 to bg-uni-green2-light bg-clip-text text-transparent border-b-2 border-dashed pb-2 animate-slidein800">
-          Introduction:
+          Overview:
         </h2>
         {eventBackground.introduction.map((intro, index) => (
-          <p key={index} className="text-base md:text-lg md:italic mb-6 text-left w-full animate-slidein900">
+          <p key={index} className="text-base md:text-lg md:italic mb-6 text-left w-full animate-slidein900 md:text-justify">
             {intro}
           </p>
         ))}
